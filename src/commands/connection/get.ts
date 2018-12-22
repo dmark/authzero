60b1a960efd6900a10371ca652c7f76ea886367b
Mark Drummond <dmark@users.noreply.github.com>
2018-12-22 17:06:44 -0500

    Adds connection:get sub-command

---
+++ src/commands/connection/get.ts
@@ -0,0 +1,38 @@
+import {Command, flags} from '@oclif/command'
+
+var dotenv = require('dotenv');
+var ManagementClient = require('auth0').ManagementClient;
+
+export default class Connection extends Command {
+  static description = 'Get connection settings.'
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    cxn_id: flags.string({char: 'i', description: 'Connection ID', required: true})
+  }
+
+  //static args = [{name: 'file'}]
+  static args = []
+
+  async run() {
+    const {args, flags} = this.parse(Connection)
+    const CXN_ID = flags.cxn_id
+    
+    dotenv.load();
+
+    var auth0 = new ManagementClient({
+      domain: process.env.AUTH0_DOMAIN,
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      scope: 'read:users'
+    });
+
+    auth0.connections.get({ id: CXN_ID }, function (err: any, settings: any) {
+      if (err) {
+        // do a thing
+      }
+
+      console.log(settings);
+    });
+  }
+}
