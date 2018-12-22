7fe123d5355de0f53304442ef22ed4503c08131c
Mark Drummond <dmark@users.noreply.github.com>
2018-12-22 16:06:54 -0500

    Updates user:get test

--- test/commands/user/get.test.ts
+++ test/commands/user/get.test.ts
@@ -4,7 +4,7 @@ describe('user:get', function() {
   test
     .stdout()
     .command(['user:get', '--user_id', 'auth0|5be194e2c0408874f867caab'])
-    .it('runs user:get --user_id=\'auth0|5be194e2c0408874f867caab\'', function(done) {
-      expect(done).to.contain('auth0|5be194e2c0408874f867caab')
+    .it('should return a user profile with user_id=\'auth0|5be194e2c0408874f867caab\'', function(done) {
+      expect({}).to.contain('user_id: \'auth0|5be194e2c0408874f867caab\'')
     })
 })
