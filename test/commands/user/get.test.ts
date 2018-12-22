141bed0ee96b227888d5f133266afd2c43942913
Mark Drummond <dmark@users.noreply.github.com>
2018-12-22 16:13:59 -0500

    Updates user:get test

--- test/commands/user/get.test.ts
+++ test/commands/user/get.test.ts
@@ -1,10 +1,10 @@
 import {expect, test} from '@oclif/test'
 
-describe('user:get', function() {
-  test
-    .stdout()
-    .command(['user:get', '--user_id', 'auth0|5be194e2c0408874f867caab'])
-    .it('should return a user profile with user_id=\'auth0|5be194e2c0408874f867caab\'', function(done) {
-      expect({}).to.contain('user_id: \'auth0|5be194e2c0408874f867caab\'')
-    })
+describe('user:get', () => {
+  fancy
+  .stdout()
+  .command['user:get', '--user_id', '\'auth0|5be194e2c0408874f867caab\'']
+  it('should return a user profile with user_id=\'auth0|5be194e2c0408874f867caab\'', ctx => {
+    expect(ctx.stdout).to.contain('user_id: \'auth0|5be194e2c0408874f867caab\'');
+  })
 })
