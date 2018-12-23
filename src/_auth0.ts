d3426354b573379c684f74afd940725efa606194
Mark Drummond <dmark@users.noreply.github.com>
2018-12-23 13:17:55 -0500

    Removes extraneaous parameters from call to ManagementClient.

--- src/_auth0.ts
+++ src/_auth0.ts
@@ -5,8 +5,6 @@ export function connectToAuth0() {
     return new ManagementClient({
         domain: process.env.AUTH0_DOMAIN,
         clientId: process.env.AUTH0_CLIENT_ID,
-        clientSecret: process.env.AUTH0_CLIENT_SECRET,
-        audience: 'https://' + process.env.AUTH0_DOMAIN + '/api/v2/',
-        scope: 'read:users'
+        clientSecret: process.env.AUTH0_CLIENT_SECRET
       });
 }
